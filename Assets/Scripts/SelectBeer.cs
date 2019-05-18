3c2f23e2ac3bf74e9b8a7354915fdd289c0bf938
Rok Kos <rok.kos@outfit7.com>
2019-05-18 22:22:31 +0200

    Selecting beer working

--- Assets/Scripts/SelectBeer.cs
+++ Assets/Scripts/SelectBeer.cs
@@ -6,11 +6,52 @@ public class SelectBeer : MonoBehaviour
 {
 
 	[SerializeField] List<GameObject> beerUnderUniversal;
+
+	private int ind = 0;
 	// Start is called before the first frame update
 
+
+	private void Start () {
+		foreach (GameObject beer in beerUnderUniversal) {
+			beer.SetActive(false);
+		}
+
+		beerUnderUniversal[ind].SetActive(true);
+
+
+	}
+
+	private void Update () {
+#if UNITY_EDITOR
+		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			SwichToLeftBeer();
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			SwichToRightBeer();
+		}
+
+#endif
+	}
+
 	public void SwichToLeftBeer () {
+		beerUnderUniversal[ind].SetActive(false);
+		ind--;
+		if (ind < 0) {
+			ind = beerUnderUniversal.Count - 1;
+		}
+
+		beerUnderUniversal[ind].SetActive(true);
+
 	}
 
 	public void SwichToRightBeer () {
+
+		beerUnderUniversal[ind].SetActive(false);
+		ind++;
+		if (ind >= beerUnderUniversal.Count) {
+			ind = 0;
+		}
+
+		beerUnderUniversal[ind].SetActive(true);
 	}
 }

fcf68f8031c169d22e9ea658cd28221e1ceb4c41
Rok Kos <rok.kos@outfit7.com>
2019-05-18 22:11:12 +0200

    Interface for selecting beer

---
+++ Assets/Scripts/SelectBeer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectBeer : MonoBehaviour
+{
+
+	[SerializeField] List<GameObject> beerUnderUniversal;
+	// Start is called before the first frame update
+
+	public void SwichToLeftBeer () {
+	}
+
+	public void SwichToRightBeer () {
+	}
+}

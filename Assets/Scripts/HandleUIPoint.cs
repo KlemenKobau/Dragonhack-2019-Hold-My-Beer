d13bf7b850e430d84abe509ebaac6d9be7efa4b0
Klemen Kobau <klemen.kobau@gmail.com>
2019-05-18 17:02:30 +0200

    Camera Rotation

---
+++ Assets/Scripts/HandleUIPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandleUIPoint : MonoBehaviour
+{
+    private Vector3 screenPos;
+    public GameObject panel;
+
+    // Update is called once per frame
+    void Update()
+    {
+//        screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        panel.transform.rotation = Camera.main.transform.rotation;
+
+    }
+}

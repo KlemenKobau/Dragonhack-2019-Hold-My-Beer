fee2a1226df2bc5d5b1fe4d6f6d60913588223d8
Rok Kos <rok.kos@outfit7.com>
2019-05-19 00:04:01 +0200

    Selecting colors from

--- Assets/Scripts/BeerColorController.cs
+++ Assets/Scripts/BeerColorController.cs
@@ -23,10 +23,30 @@ public class BeerColorController : MonoBehaviour
  {13, "d77f59"},
  {14, "94523a"},
  {15, "94523a"},
+ {16, "94523a"},
+ {17, "804541"},
  {18, "804541"},
+ {19, "804541"},
  {20, "5b342f"},
+ {21, "5b342f"},
+ {22, "5b342f"},
+ {23, "4c3b2b"},
  {24, "4c3b2b"},
+ {25, "4c3b2b"},
+ {26, "4c3b2b"},
+ {27, "38302e"},
+ {28, "38302e"},
+ {29, "38302e"},
  {30, "38302e"},
+ {31, "38302e"},
+ {32, "38302e"},
+ {33, "38302e"},
+ {34, "38302e"},
+ {35, "38302e"},
+ {36, "31302c"},
+ {37, "31302c"},
+ {38, "31302c"},
+ {39, "31302c"},
  {40, "31302c"}};
 
 
@@ -40,13 +60,13 @@ public class BeerColorController : MonoBehaviour
 		float.TryParse(srm, out aprox);
 		int final_value = Mathf.RoundToInt(aprox);
 
-
 		string hex;
 		srmToColor.TryGetValue(final_value, out hex);
+		hex = "#" + hex;
 		Color newCol;
 		if (ColorUtility.TryParseHtmlString(hex, out newCol))
 
-		mat.SetColor("Tint", newCol);
+		mat.SetColor("_Tint", newCol);
     }
 
     // Update is called once per frame

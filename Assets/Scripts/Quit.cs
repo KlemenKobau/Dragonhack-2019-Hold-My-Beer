85d4fc0eb2f2b57f35abf3d76c5ab3d84e446b95
Rok Kos <rok.kos@outfit7.com>
2019-05-19 10:43:16 +0200

    Build Fix

--- Assets/Scripts/Quit.cs
+++ Assets/Scripts/Quit.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GoBack : MonoBehaviour
+public class Quit : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
